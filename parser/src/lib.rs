d6dcb446aef058d20cb9940325b56e37d31d3847
Theo Paris <theoparisdesigns@gmail.com>
2022-06-18 23:27:23 -0700

    style: fix parser formatting

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -72,34 +72,35 @@ impl std::fmt::Display for Instruction {
 pub fn value() -> impl Parser<char, Instruction, Error = Simple<char>> {
 	recursive(|value| {
 		let quote = choice((just('"'), just('\'')));
-		let escape = just('\\').ignore_then(
-			just('\\')
-				.or(just('/'))
-				.or(just('"'))
-				.or(just('b').to('\x08'))
-				.or(just('f').to('\x0C'))
-				.or(just('n').to('\n'))
-				.or(just('r').to('\r'))
-				.or(just('t').to('\t'))
-				.or(just('u').ignore_then(
-					filter(|c: &char| c.is_ascii_hexdigit())
-						.repeated()
-						.exactly(4)
-						.collect::<String>()
-						.validate(|digits, span, emit| {
-							char::from_u32(
-								u32::from_str_radix(&digits, 16).unwrap(),
-							)
-							.unwrap_or_else(|| {
-								emit(Simple::custom(
-									span,
-									"invalid unicode character",
-								));
-								'\u{FFFD}' // unicode replacement character
-							})
-						}),
-				)),
-		);
+		let escape =
+			just('\\').ignore_then(
+				just('\\')
+					.or(just('/'))
+					.or(just('"'))
+					.or(just('b').to('\x08'))
+					.or(just('f').to('\x0C'))
+					.or(just('n').to('\n'))
+					.or(just('r').to('\r'))
+					.or(just('t').to('\t'))
+					.or(just('u').ignore_then(
+						filter(|c: &char| c.is_ascii_hexdigit())
+							.repeated()
+							.exactly(4)
+							.collect::<String>()
+							.validate(|digits, span, emit| {
+								char::from_u32(
+									u32::from_str_radix(&digits, 16).unwrap(),
+								)
+								.unwrap_or_else(|| {
+									emit(Simple::custom(
+										span,
+										"invalid unicode character",
+									));
+									'\u{FFFD}' // unicode replacement character
+								})
+							}),
+					)),
+			);
 
 		let string = filter(|c| *c != '\\' && *c != '"' && *c != '\'')
 			.or(escape)
